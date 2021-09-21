b8feb9d3bd69f8705a12f1ce2da95268681e24b9
rbozburun <59255765+rbozburun@users.noreply.github.com>
2021-09-22 00:56:05 +0300

    Create mailSender.py

---
+++ mailSender.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+import sys
+import smtplib
+import argparse
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+def main():
+    parser = argparse.ArgumentParser(description="HTML mail template whcih will send.")
+    parser.add_argument('-mf','--message_file', help="HTML mail template.")
+    parser.add_argument('-r','--recipient', help="Recipient(s) emaill address. You can use comma (,) for multiple recipients.")
+    parser.add_argument('-tm', '--test_mode', help="If you want to test script without any CC you can run test mode.")
+    parser.add_argument('-img', '--image', help="Image(s) in HTML. You can use comma seperator. USAGE: -img image.png", )
+    parser.add_argument('-t', '--type', help="Type of email. AVAIBLE: \{leak, ransomware_digest, vulnerability_digest\}", required=True)
+    args = parser.parse_args()
+
+    recipients = args.recipient
+    smtp = connect()
+
+    msg = MIMEMultipart("alternative")
+    subject = ""
+    msg_from = ""
+
+    if args.type == "leak":
+        msg_from = "\"Intel\" Report <intelreport@threatmonit.io>"
+        subject = "Cyber Threat Intelligence Report"
+
+    elif args.type == "ransomware_digest":
+        msg_from = "\"Ransomware Digest\" <intelreport@threatmonit.io>"
+        subject = "ThreatMonIT Weekly Ransomware Digest Report"
+
+    elif args.type == "vulnerability_digest":
+        msg_from = "\"Vulnerability Digest\" <intelreport@threatmonit.io>"
+        subject = "ThreatMonIT Weekly Vulnerability Digest Report"
+    
+    else:
+        print("Wrong type !!! Exiting.")
+        return
+
+    msg["Subject"] = subject
+    msg["From"] = msg_from
+    msg["To"] = recipients
+
+    #Check is test mode?
+    if args.test_mode is not None:
+        print("Script running on test mode, there is no  user in CC.")
+    else:    
+        msg["CC"] = "archive@infinitumit.com.tr"
+
+    #Create Message body
+    text = MIMEText(open(args.message_file, 'r', encoding='utf8').read(), 'html')
+    #Attach text to msg
+    msg.attach(text)
+
+    #Check there is any image?
+    if args.image is not None:
+        img_part = MIMEImage(open(args.image, 'rb').read())
+        img_part.add_header('Content-ID', "<{}>".format(args.image))
+        #Attach img
+        msg.attach(img_part)
+
+    smtp.sendmail(msg_from, recipients.split(","), msg.as_string())
+    smtp.quit()
+
+
+def connect():
+    smtp_user = ""
+    smtp_pass = ""
+
+    try:
+        smtp = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(smtp_user, smtp_pass)
+        print("Connected to smtp server...")
+        return smtp
+        
+
+    except Exception as e:
+        exc_type, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print("!!!! ERROR !!!!")
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print("There is an error occured: {0}".format(e))
+
+   
+if __name__ == "__main__":
+    main()
